17cb2506c0ccc7a2e2ab24f6536650a9689ee45b
Adrian Sims <adriansims62781@gmail.com>
2020-09-18 17:29:00 +0200

    updates.

--- SqlSnip1/Program.cs
+++ SqlSnip1/Program.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SqlSnip1
 {
     class Program
     {
-        /// <summary>
-        /// Return a result set as a List of Lists
-        /// </summary>
-        /// <param name="server">database server</param>
-        /// <param name="dbName">database name</param>
-        /// <param name="sql">sql to run</param>
-        /// <param name="sqlParams">parameters for the query</param>
-        /// <returns>A list of lists</returns>
-        public static List<List<object>> GetResults(string server, string dbName, string sql, SqlParameter[] sqlParams)
+        public static int SqlNonQuery(string server, string dbName, string sql, SqlParameter[] sqlParams)
         {
             var builder = new SqlConnectionStringBuilder
             {
@@ -23,12 +16,56 @@ namespace SqlSnip1
                 IntegratedSecurity = true
             };
 
-            var list = new List<List<object>>();
             using (var connection = new SqlConnection(builder.ToString()))
             using (var command = new SqlCommand(sql, connection))
             {
                 connection.Open();
                 command.Parameters.AddRange(sqlParams);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public static DataTable RunStoredProcedureGetDataTable(string server, string dbName, string procname, SqlParameter[] sqlParams)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(procname, connection))
+            {
+                connection.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddRange(sqlParams);
+                var adapter = new SqlDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+
+
+        public static List<List<object>> RunStoredProcedureGetList(string server, string dbName, string procname, SqlParameter[] sqlParams)
+        {
+            var list = new List<List<object>>();
+
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(procname, connection))
+            {
+                connection.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddRange(sqlParams);
+                var adapter = new SqlDataAdapter(command);
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
@@ -41,22 +78,66 @@ namespace SqlSnip1
                             innerList.Add(reader[i]);
                         }
                     }
+                    return list;
                 }
             }
-            return list;
+        }
+        public static object RunStoredProcedure(string server, string dbName, string procname, SqlParameter[] sqlParams)
+        {
+            var list = new List<List<object>>();
+
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(procname, connection))
+            {
+                connection.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddRange(sqlParams);
+                return command.ExecuteScalar();
+            }
+        }
+
+        public static void RunStoredProcedureGetScalar(string server, string dbName, string procname, SqlParameter[] sqlParams)
+        {
+
+        }
+
+
+        // test function - remove it as you will not need it!
+        private static void TestIt()
+        {
+            var nq = $@"CREATE OR ALTER PROCEDURE LOGINFO @Message NVARCHAR(100) AS
+                        BEGIN
+	                        SELECT CONCAT(N'INFO:', @Message)
+                        END
+                        ";
+            var result = SqlNonQuery(@"(localdb)\db1", "db1a", nq, new SqlParameter[] { });
+
+            var nq2 = $@"CREATE OR ALTER PROCEDURE STOREINFO @Message NVARCHAR(100) AS
+                        BEGIN
+	                        INSERT INTO LIST VALUES(@Message)
+                        END
+                        ";
+            var result2 = SqlNonQuery(@"(localdb)\db1", "db1a", nq2, new SqlParameter[] { });
+
+
+            var dataTable = RunStoredProcedureGetDataTable(@"(localdb)\db1", "db1a", "LOGINFO", new[] { new SqlParameter("Message", "Hello") });
+            var list = RunStoredProcedureGetList(@"(localdb)\db1", "db1a", "LOGINFO", new[] { new SqlParameter("Message", "Hello") });
+            RunStoredProcedure(@"(localdb)\db1", "db1a", "STOREINFO", new[] { new SqlParameter("Message", "Hello") });
         }
 
         private static void Main(string[] args)
         {
             try
             {
-                var results = GetResults(@"(localdb)\db1", "db1a", "select * from t1 where Name=@Name", new[] { new SqlParameter("Name", "One") });
-                foreach (var i in results)
-                {
-                    var line = string.Join(" ", i);
-                    Console.WriteLine($"{line}");
-                }
-                Console.WriteLine("EXIT!");
+
+                TestIt();
             }
             catch (Exception ex)
             {
--- SqlSnip1/X.cs
+++ SqlSnip1/X.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 namespace SqlSnip1
 {
     class X
     {
+        public static List<List<object>> GetResults(string server, string dbName, string sql, SqlParameter[] sqlParams)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            var list = new List<List<object>>();
+            using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.AddRange(sqlParams);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var innerList = new List<object>();
+                        list.Add(innerList);
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            innerList.Add(reader[i]);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+        void TestGetResults()
+        {
+            var results = GetResults(@"(localdb)\db1", "db1a", "select * from t1 where Name=@Name", new[] { new SqlParameter("Name", "One") });
+            foreach (var i in results)
+            {
+                var line = string.Join(" ", i);
+                Console.WriteLine($"{line}");
+            }
+        }
+
         public static DataTable GetTable(string server, string dbName, string sql, SqlParameter[] sqlParams)
         {
             var builder = new SqlConnectionStringBuilder
@@ -15,16 +58,14 @@ namespace SqlSnip1
             };
 
             using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(sql, connection))
             {
                 connection.Open();
-                using (var command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddRange(sqlParams);
-                    var adapter = new SqlDataAdapter(command);
-                    var dataTable = new DataTable();
-                    adapter.Fill(dataTable);
-                    return dataTable;
-                }
+                command.Parameters.AddRange(sqlParams);
+                var adapter = new SqlDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
             }
         }
 
@@ -38,6 +79,33 @@ namespace SqlSnip1
                     Console.WriteLine($"{col}");
                 }
             }
+        }
+        public static int SqlNonQuery(string server, string dbName, string sql, SqlParameter[] sqlParams)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.AddRange(sqlParams);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        // test function - remove it as you will not need it!
+        private static void TestNonQuery()
+        {
+            var nq = $@"DECLARE @sql nvarchar(500)
+                        SET @sql = Convert(nvarchar(500), CONCAT('CREATE TABLE ', @tablename, '(id int)'))
+                        exec sp_executesql  @sql";
+
+            var result = SqlNonQuery(@"(localdb)\db1", "db1a", nq, new[] { new SqlParameter("tablename", "Mytable001e") });
         }
     }
 }
