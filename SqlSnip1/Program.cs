2ba9e810d92a83da961b23b6f386be9e5c501a5d
Adrian Sims <adriansims62781@gmail.com>
2020-09-18 14:20:52 +0200

    Add project files.

---
+++ SqlSnip1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace SqlSnip1
+{
+    class Program
+    {
+        /// <summary>
+        /// Return a result set as a List of Lists
+        /// </summary>
+        /// <param name="server">database server</param>
+        /// <param name="dbName">database name</param>
+        /// <param name="sql">sql to run</param>
+        /// <param name="sqlParams">parameters for the query</param>
+        /// <returns>A list of lists</returns>
+        public static List<List<object>> GetResults(string server, string dbName, string sql, SqlParameter[] sqlParams)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            var list = new List<List<object>>();
+            using (var connection = new SqlConnection(builder.ToString()))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.AddRange(sqlParams);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var innerList = new List<object>();
+                        list.Add(innerList);
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            innerList.Add(reader[i]);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var results = GetResults(@"(localdb)\db1", "db1a", "select * from t1 where Name=@Name", new[] { new SqlParameter("Name", "One") });
+                foreach (var i in results)
+                {
+                    var line = string.Join(" ", i);
+                    Console.WriteLine($"{line}");
+                }
+                Console.WriteLine("EXIT!");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error: {ex}");
+            }
+        }
+    }
+}
---
+++ SqlSnip1/X.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace SqlSnip1
+{
+    class X
+    {
+        public static DataTable GetTable(string server, string dbName, string sql, SqlParameter[] sqlParams)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+
+            using (var connection = new SqlConnection(builder.ToString()))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddRange(sqlParams);
+                    var adapter = new SqlDataAdapter(command);
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+
+        private static void TestGetTable()
+        {
+            var table = GetTable(@"(localdb)\db1", "db1a", "select * from t1 where Name=@Name", new[] { new SqlParameter("Name", "One") });
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var col in row.ItemArray)
+                {
+                    Console.WriteLine($"{col}");
+                }
+            }
+        }
+    }
+}
